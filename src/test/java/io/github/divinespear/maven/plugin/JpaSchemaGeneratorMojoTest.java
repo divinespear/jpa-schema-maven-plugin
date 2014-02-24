6f666b2c52783c070055521e73f9990d3bd820f8
Sin-young "Divinespear" Kang <divinespear@gmail.com>
2014-02-24 15:31:15 +0900

    issue #7 - modified sql compare, added format sql test

--- src/test/java/io/github/divinespear/maven/plugin/JpaSchemaGeneratorMojoTest.java
+++ src/test/java/io/github/divinespear/maven/plugin/JpaSchemaGeneratorMojoTest.java
@@ -19,7 +19,6 @@ package io.github.divinespear.maven.plugin;
  * under the License.
  */
 
-import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.anyOf;
@@ -36,10 +35,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
@@ -110,20 +107,6 @@ public class JpaSchemaGeneratorMojoTest
         return mojo;
     }
 
-    private List<String> readFileAsList(File file) throws IOException {
-        List<String> list = new ArrayList<String>();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        try {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                list.add(line);
-            }
-        } finally {
-            reader.close();
-        }
-        return list;
-    }
-
     private String readFileAsText(File file) throws IOException {
         StringBuilder builder = new StringBuilder();
         BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -151,26 +134,21 @@ public class JpaSchemaGeneratorMojoTest
         this.compileJpaModelSources(pomfile);
         JpaSchemaGeneratorMojo mojo = this.executeSchemaGeneration(pomfile);
 
-        // file check
-        List<String> lines = null;
-
         File createScriptFile = mojo.getCreateOutputFile();
         assertThat("create script should be generated.", createScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(createScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectCreate = "CREATE TABLE KEY_VALUE_STORE (STORED_KEY VARCHAR(128) NOT NULL, CREATED_AT TIMESTAMP, STORED_VALUE VARCHAR(32768), PRIMARY KEY (STORED_KEY));\r\n"
+                                    + "CREATE TABLE MANY_COLUMN_TABLE (ID BIGINT NOT NULL, COLUMN00 VARCHAR, COLUMN01 VARCHAR, COLUMN02 VARCHAR, COLUMN03 VARCHAR, COLUMN04 VARCHAR, COLUMN05 VARCHAR, COLUMN06 VARCHAR, COLUMN07 VARCHAR, COLUMN08 VARCHAR, COLUMN09 VARCHAR, COLUMN10 VARCHAR, COLUMN11 VARCHAR, COLUMN12 VARCHAR, COLUMN13 VARCHAR, COLUMN14 VARCHAR, COLUMN15 VARCHAR, COLUMN16 VARCHAR, COLUMN17 VARCHAR, COLUMN18 VARCHAR, COLUMN19 VARCHAR, COLUMN20 VARCHAR, COLUMN21 VARCHAR, COLUMN22 VARCHAR, COLUMN23 VARCHAR, COLUMN24 VARCHAR, COLUMN25 VARCHAR, COLUMN26 VARCHAR, COLUMN27 VARCHAR, COLUMN28 VARCHAR, COLUMN29 VARCHAR, PRIMARY KEY (ID));\r\n"
+                                    + "CREATE SEQUENCE SEQ_GEN_SEQUENCE INCREMENT BY 50 START WITH 50;\r\n";
+        assertThat(this.readFileAsText(createScriptFile), is(expectCreate));
 
         File dropScriptFile = mojo.getDropOutputFile();
         assertThat("drop script should be generated.", dropScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(dropScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectDrop = "DROP TABLE KEY_VALUE_STORE;\r\n"
+                                  + "DROP TABLE MANY_COLUMN_TABLE;\r\n"
+                                  + "DROP SEQUENCE SEQ_GEN_SEQUENCE;\r\n";
+        assertThat(this.readFileAsText(dropScriptFile), is(expectDrop));
     }
 
     /**
@@ -185,9 +163,6 @@ public class JpaSchemaGeneratorMojoTest
 
         this.compileJpaModelSources(pomfile);
         JpaSchemaGeneratorMojo mojo = this.executeSchemaGeneration(pomfile);
-
-        // file check
-        List<String> lines = null;
 
         File createScriptFile = mojo.getCreateOutputFile();
         assertThat("create script should be generated.", createScriptFile.exists(), is(true));
@@ -238,11 +213,10 @@ public class JpaSchemaGeneratorMojoTest
         File dropScriptFile = mojo.getDropOutputFile();
         assertThat("drop script should be generated.", dropScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(dropScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectDrop = "DROP TABLE KEY_VALUE_STORE;\r\n"
+                                  + "DROP TABLE MANY_COLUMN_TABLE;\r\n"
+                                  + "DROP SEQUENCE SEQ_GEN_SEQUENCE;\r\n";
+        assertThat(this.readFileAsText(dropScriptFile), is(expectDrop));
     }
 
     /**
@@ -312,26 +286,21 @@ public class JpaSchemaGeneratorMojoTest
         this.compileJpaModelSources(pomfile);
         JpaSchemaGeneratorMojo mojo = this.executeSchemaGeneration(pomfile);
 
-        // file check
-        List<String> lines = null;
-
         File createScriptFile = mojo.getCreateOutputFile();
         assertThat("create script should be generated.", createScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(createScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectCreate = "create table key_value_store (stored_key varchar(128) not null, created_at timestamp, stored_value varchar(32768), primary key (stored_key));\r\n"
+                                    + "create table many_column_table (id bigint not null, column00 varchar(255), column01 varchar(255), column02 varchar(255), column03 varchar(255), column04 varchar(255), column05 varchar(255), column06 varchar(255), column07 varchar(255), column08 varchar(255), column09 varchar(255), column10 varchar(255), column11 varchar(255), column12 varchar(255), column13 varchar(255), column14 varchar(255), column15 varchar(255), column16 varchar(255), column17 varchar(255), column18 varchar(255), column19 varchar(255), column20 varchar(255), column21 varchar(255), column22 varchar(255), column23 varchar(255), column24 varchar(255), column25 varchar(255), column26 varchar(255), column27 varchar(255), column28 varchar(255), column29 varchar(255), primary key (id));\r\n"
+                                    + "create sequence hibernate_sequence;\r\n";
+        assertThat(this.readFileAsText(createScriptFile), is(expectCreate));
 
         File dropScriptFile = mojo.getDropOutputFile();
         assertThat("drop script should be generated.", dropScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(dropScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectDrop = "drop table key_value_store if exists;\r\n"
+                                  + "drop table many_column_table if exists;\r\n"
+                                  + "drop sequence hibernate_sequence;\r\n";
+        assertThat(this.readFileAsText(dropScriptFile), is(expectDrop));
     }
 
     /**
@@ -346,26 +315,59 @@ public class JpaSchemaGeneratorMojoTest
         this.compileJpaModelSources(pomfile);
         JpaSchemaGeneratorMojo mojo = this.executeSchemaGeneration(pomfile);
 
-        // file check
-        List<String> lines = null;
-
         File createScriptFile = mojo.getCreateOutputFile();
         assertThat("create script should be generated.", createScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(createScriptFile);
-        assertThat(lines.size(), is(41));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectCreate = "create table key_value_store (\r\n"
+                                    + "\tstored_key varchar(128) not null,\r\n"
+                                    + "\tcreated_at timestamp,\r\n"
+                                    + "\tstored_value varchar(32768),\r\n"
+                                    + "\tprimary key (stored_key)\r\n"
+                                    + ");\r\n"
+                                    + "create table many_column_table (\r\n"
+                                    + "\tid bigint not null,\r\n"
+                                    + "\tcolumn00 varchar(255),\r\n"
+                                    + "\tcolumn01 varchar(255),\r\n"
+                                    + "\tcolumn02 varchar(255),\r\n"
+                                    + "\tcolumn03 varchar(255),\r\n"
+                                    + "\tcolumn04 varchar(255),\r\n"
+                                    + "\tcolumn05 varchar(255),\r\n"
+                                    + "\tcolumn06 varchar(255),\r\n"
+                                    + "\tcolumn07 varchar(255),\r\n"
+                                    + "\tcolumn08 varchar(255),\r\n"
+                                    + "\tcolumn09 varchar(255),\r\n"
+                                    + "\tcolumn10 varchar(255),\r\n"
+                                    + "\tcolumn11 varchar(255),\r\n"
+                                    + "\tcolumn12 varchar(255),\r\n"
+                                    + "\tcolumn13 varchar(255),\r\n"
+                                    + "\tcolumn14 varchar(255),\r\n"
+                                    + "\tcolumn15 varchar(255),\r\n"
+                                    + "\tcolumn16 varchar(255),\r\n"
+                                    + "\tcolumn17 varchar(255),\r\n"
+                                    + "\tcolumn18 varchar(255),\r\n"
+                                    + "\tcolumn19 varchar(255),\r\n"
+                                    + "\tcolumn20 varchar(255),\r\n"
+                                    + "\tcolumn21 varchar(255),\r\n"
+                                    + "\tcolumn22 varchar(255),\r\n"
+                                    + "\tcolumn23 varchar(255),\r\n"
+                                    + "\tcolumn24 varchar(255),\r\n"
+                                    + "\tcolumn25 varchar(255),\r\n"
+                                    + "\tcolumn26 varchar(255),\r\n"
+                                    + "\tcolumn27 varchar(255),\r\n"
+                                    + "\tcolumn28 varchar(255),\r\n"
+                                    + "\tcolumn29 varchar(255),\r\n"
+                                    + "\tprimary key (id)\r\n"
+                                    + ");\r\n"
+                                    + "create sequence hibernate_sequence;\r\n";
+        assertThat(this.readFileAsText(createScriptFile), is(expectCreate));
 
         File dropScriptFile = mojo.getDropOutputFile();
         assertThat("drop script should be generated.", dropScriptFile.exists(), is(true));
 
-        lines = this.readFileAsList(dropScriptFile);
-        assertThat(lines.size(), is(3));
-        for (String line : lines) {
-            assertThat(line, endsWith(";"));
-        }
+        final String expectDrop = "drop table key_value_store if exists;\r\n"
+                                  + "drop table many_column_table if exists;\r\n"
+                                  + "drop sequence hibernate_sequence;\r\n";
+        assertThat(this.readFileAsText(dropScriptFile), is(expectDrop));
     }
 
     /**
