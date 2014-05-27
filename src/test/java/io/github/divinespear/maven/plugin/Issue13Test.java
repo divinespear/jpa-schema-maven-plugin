d7860e6390b4b8956825670c6b77f60972738dd1
Sin-young "Divinespear" Kang <divinespear@gmail.com>
2014-05-27 10:31:45 +0900

    issue #13: added test for issue 13.

---
+++ src/test/java/io/github/divinespear/maven/plugin/Issue13Test.java
@@ -0,0 +1,20 @@
+package io.github.divinespear.maven.plugin;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Issue13Test
+        extends AbstractSchemaGeneratorMojoTest {
+
+    @Before
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @After
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
